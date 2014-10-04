4399ce9c8557ab3700e010eeb9036a13ab1c80c2
Vincent Jacques <vincent@vincent-jacques.net>
2014-10-04 15:00:44 -0700

    Support same versions of Python as Travis-CI

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Environment :: Web Environment",
         ],
