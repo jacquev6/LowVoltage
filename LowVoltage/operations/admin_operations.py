4a5364db73dad4f9a1a5c65b3410f0a44b711d29
Vincent Jacques <vincent@vincent-jacques.net>
2014-10-18 14:29:04 -0700

    Complete ListTable

--- LowVoltage/operations/admin_operations.py
+++ LowVoltage/operations/admin_operations.py
@@ -16,14 +16,38 @@ class ListTables(Operation):
 
     def __init__(self):
         super(ListTables, self).__init__("ListTables")
+        self.__limit = None
+        self.__exclusive_start_table_name = None
+
+    def limit(self, limit):
+        self.__limit = limit
+        return self
+
+    def exclusive_start_table_name(self, table_name):
+        self.__exclusive_start_table_name = table_name
+        return self
 
     def build(self):
         data = {}
+        if self.__limit:
+            data["Limit"] = str(self.__limit)
+        if self.__exclusive_start_table_name:
+            data["ExclusiveStartTableName"] = self.__exclusive_start_table_name
         return data
 
 
 class ListTablesUnitTests(unittest.TestCase):
-    pass
+    def testName(self):
+        self.assertEqual(ListTables().name, "ListTables")
+
+    def testNoArguments(self):
+        self.assertEqual(ListTables().build(), {})
+
+    def testLimit(self):
+        self.assertEqual(ListTables().limit(42).build(), {"Limit": "42"})
+
+    def testExclusiveStartTableName(self):
+        self.assertEqual(ListTables().exclusive_start_table_name("Bar").build(), {"ExclusiveStartTableName": "Bar"})
 
 
 class ListTablesIntegTests(LowVoltage.tests.dynamodb_local.TestCase):
@@ -31,6 +55,43 @@ class ListTablesIntegTests(LowVoltage.tests.dynamodb_local.TestCase):
         r = self.connection.request(ListTables())
         self.assertEqual(r.table_names, [])
         self.assertEqual(r.last_evaluated_table_name, None)
+
+    def testAllArguments(self):
+        self.connection.request(
+            "CreateTable",
+            {
+                "TableName": "Aaa",
+                "AttributeDefinitions": [{"AttributeName": "hash", "AttributeType": "S"}],
+                "KeySchema":[{"AttributeName":"hash", "KeyType":"HASH"}],
+                "ProvisionedThroughput": {"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+            }
+        )
+        self.connection.request(
+            "CreateTable",
+            {
+                "TableName": "Bbb",
+                "AttributeDefinitions": [{"AttributeName": "hash", "AttributeType": "S"}],
+                "KeySchema":[{"AttributeName":"hash", "KeyType":"HASH"}],
+                "ProvisionedThroughput": {"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+            }
+        )
+        self.connection.request(
+            "CreateTable",
+            {
+                "TableName": "Ccc",
+                "AttributeDefinitions": [{"AttributeName": "hash", "AttributeType": "S"}],
+                "KeySchema":[{"AttributeName":"hash", "KeyType":"HASH"}],
+                "ProvisionedThroughput": {"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+            }
+        )
+
+        r = self.connection.request(ListTables().exclusive_start_table_name("Aaa").limit(1))
+        self.assertEqual(r.table_names, ["Bbb"])
+        self.assertEqual(r.last_evaluated_table_name, "Bbb")
+
+        self.connection.request("DeleteTable", {"TableName": "Aaa"})
+        self.connection.request("DeleteTable", {"TableName": "Bbb"})
+        self.connection.request("DeleteTable", {"TableName": "Ccc"})
 
 
 if __name__ == "__main__":
