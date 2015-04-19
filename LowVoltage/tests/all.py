ce3637fd196ffe2373a755d835d4e39964e31aff
Vincent Jacques <vincent@vincent-jacques.net>
2015-04-19 23:06:52 +0200

    Be more consistent

--- LowVoltage/tests/all.py
+++ LowVoltage/tests/all.py
@@ -7,6 +7,7 @@ import unittest
 import LowVoltage.testing.dynamodb_local
 from . import *
 
+
 if __name__ == "__main__":  # pragma no branch (Test code)
     with LowVoltage.testing.dynamodb_local.DynamoDbLocal():  # pragma no cover (Test code)
         unittest.main()  # pragma no cover (Test code)
